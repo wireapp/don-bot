b2e1e8d16c50904e59364cf968fe2675360dc2be
dkovacevic <dejankov@gmail.com>
2020-04-06 15:58:17 +0200

    Fixed the glitch No2

--- src/main/java/com/wire/bots/don/DAO/ServiceDAO.java
+++ src/main/java/com/wire/bots/don/DAO/ServiceDAO.java
@@ -7,11 +7,13 @@ import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.Define;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+import org.skife.jdbi.v2.sqlobject.stringtemplate.UseStringTemplate3StatementLocator;
 import org.skife.jdbi.v2.tweak.ResultSetMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@UseStringTemplate3StatementLocator
 public interface ServiceDAO {
     @SqlUpdate("INSERT INTO Service (Name) VALUES(:name)")
     int insertService(@Bind("name") String name);
